6cbeb7d5f9c50e0796b69215472a44bff706a427
peng <peng8755@hotmail.com>
2021-05-07 18:40:00 +0800

    add animals function

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::apiResource('animals',AnimalController::class);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
