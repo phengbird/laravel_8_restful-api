b7e0fa75027b5f64b6f01ca4adae572120a8a051
peng <peng8755@hotmail.com>
2021-05-09 00:05:09 +0800

    type

---
+++ app/Http/Controllers/TypeController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpFoundation\Response;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $types = Type::get();
+        return response([
+            'data' => $types,
+        ],Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request , [
+            'name' => [
+                'require',
+                'max:50',
+                Rule::unique('types','name')//check is unique 
+            ],
+            ,'sort' => 'nullable|integer'
+        ]);
+
+        if(!isset($request->sort)){
+            $max = Type::max('sort');
+            $request['sort'] = $max + 1;
+        }
+        $type = Type::create($request->all()); //store to dbs
+        $type = $type->refresh();
+        return response([
+            'data' => $type,
+        ],Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Type $type)
+    {
+        //
+        return response([
+            'data' => $type
+        ],Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Type $type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Type $type)
+    {
+        //
+        $this->validate($request , [
+            'name' => [
+                'max:50',
+                Rule::unique('types','name')->ignore($type->name,'name') //to check same to old or unique
+            ],
+            'sort' => 'nullable|integer'
+        ]);
+
+        $type->update($request->all());
+        return response([
+            'data' => $type
+        ],Response::HTTP_OK);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Type $type)
+    {
+        //
+        $type->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use App\Http\Controllers\AnimalController;
 */
 
 Route::apiResource('animals',AnimalController::class);
+Route::apiResource('Types',TypeController::class);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
